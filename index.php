020cc25703c56b623390361a8a9a7ddc0457d1c0
Priyanka Mukund <priyanka@kunday.com>
2019-07-14 18:58:58 +1000

    create php inside html.

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-<?php
-
-$name = "Priyanka";
-
-echo "Hello, $name";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>learn php</title>
+  <style>
+    header {
+      background: pink;
+      padding: 2em;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <header>
+  <h1><?php  echo "Hello world"; ?></h1>
+  </header
+</body>
+</html>
