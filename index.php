12745a195678b1889beb80c274f8c45f9ee337e7
Priyanka Mukund <priyanka@kunday.com>
2019-07-14 19:23:53 +1000

    shifted index code to index.view.php seperation of concerns.

--- index.php
+++ index.php
@@ -1,26 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>learn php</title>
-  <style>
-    header {
-      background: pink;
-      padding: 2em;
-      text-align: center;
-    }
-  </style>
-</head>
 
-<body>
-  <header>
-    <h1>
-      <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
-    </h1>
-  </header>
-</body>
+$greeting = "Hello World";
 
-</html>
+require 'index.view.php';
---
+++ index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>learn php</title>
+  <style>
+    header {
+      background: pink;
+      padding: 2em;
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <header>
+    <h1>
+      <?= $greeting; ?>
+    </h1>
+  </header>
+</body>
+
+</html>
